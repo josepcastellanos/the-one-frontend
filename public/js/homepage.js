f9d1e8be4893b65282e12e198e3e6e4b8de93e0c
josepcastellanos <65287251+josepcastellanos@users.noreply.github.com>
2022-06-06 20:11:20 +0200

    Language changes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -12,7 +12,7 @@ const rootComponent  = {
   <h1>Select a simulation mode</h1>
   <div class="nav">
     <form class="centerButtons" action="/par" method="post">
-        <input class="nav_button" type="submit" name="" value="Parametrizado">
+        <input class="nav_button" type="submit" name="" value="Parameterized">
       </form>
     <form class="centerButtons" action="/bulk" method="post">
         <input class="nav_button" type="submit" name="" value="Bulk">
